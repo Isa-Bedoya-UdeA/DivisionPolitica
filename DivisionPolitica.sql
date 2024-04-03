193b2de177025e9c61baba2d0624635441ef1e2b
Isabela Bedoya Gaviria <isbega0406@gmail.com>
2024-04-02 22:13:18 -0500

    update

---
+++ DivisionPolitica.sql
@@ -0,0 +1,12 @@
+-- NOTA: Ejercutar este archivo (DivisionPolitica.sql) después de la función.
+
+/* PUNTO 7 DEL TALLER (LABORATORIO):
+• Normalización en la tabla Pais de la información de la Moneda
+• Agregar campos para imágenes del Mapa y la Bandera del País
+
+Isabela Bedoya Gaviria.
+*/
+
+SELECT fManagePais();
+SELECT * FROM Moneda; -- verificar que la tabla Moneda se haya creado
+SELECT * FROM Pais; -- verificar que la tabla Pais se haya modificado
